d82648b98c94d64f9faf22c437c0ecfb5be1c8fe
André Bittencourt <andr01a0@stud.kea.dk>
2022-08-15 00:51:18 +0200

    clean code

--- backend/src/routes/api.route.js
+++ backend/src/routes/api.route.js
@@ -14,20 +14,4 @@ router
   await apiController.updateProfile(req, res, next)
 })
 
-/* .get('/match', function(req, res, next) {
-  apiController.getMatches(req, res, next)
-})
-.post('/match', function(req, res, next) {
-  apiController.createMatch(req, res, next)
-})
-.get('/match/:id', function(req, res, next) {
-  apiController.getMatchById(req, res, next)
-})
-.put('/match/:id', function(req, res, next) {
-  apiController.updateMatchById(req, res, next)
-})
-.delete('/match/:id', function(req, res, next) {
-  apiController.deleteMatchById(req, res, next)
-}) */
-
 export default router
